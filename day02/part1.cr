48d2c309fb848f09dc12721172c219dc6868bbb8
Stephen Thirlwall <sdt@dr.com>
2023-12-03 10:48:26 +1100

    Day 2 further simplifications

--- day02/part1.cr
+++ day02/part1.cr
@@ -8,24 +8,25 @@ Maximum = {
 
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 def parse_game(line)
-  header, *rounds = line.split(/[:;] /)
-  id = header.match!(/^Game (\d+)/)[1].to_i
-  subsets = rounds.map do |round|
-    subset = Hash(String, Int32).new
-    round.split(", ").each do |pair|
-      count, color = pair.split(" ")
-      subset[color] = count.to_i
-    end
-    subset
+  game = {
+    "red"   => 0,
+    "green" => 0,
+    "blue"  => 0,
+  }
+
+  # Collapse all the samples together. We only care about the highest for
+  # each color.
+  _, id, *samples = line.split(/[:;,]? /)
+  samples.each_slice(2) do |iter|
+    count, color = iter
+    game[color] = [ count.to_i, game[color] ].max
   end
-  { id: id, rounds: subsets }
+  { id: id.to_i, count: game }
 end
 
 def game_is_possible(game)
-  game[:rounds].all? do |round|
-    round.all? do |color, count|
-      count <= Maximum[color]
-    end
+  game[:count].all? do |color, count|
+    count <= Maximum[color]
   end
 end
 
