640a78d94f9e3e99015758cbb9984a2186865620
Stephen Thirlwall <sdt@dr.com>
2023-12-03 10:59:16 +1100

    Day 2 refactoring

--- day02/part1.cr
+++ day02/part1.cr
@@ -7,31 +7,17 @@ Maximum = {
 }
 
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-def parse_game(line)
-  game = {
-    "red"   => 0,
-    "green" => 0,
-    "blue"  => 0,
-  }
-
-  # Collapse all the samples together. We only care about the highest for
-  # each color.
+def game_id(line)
   _, id, *samples = line.split(/[:;,]? /)
   samples.each_slice(2) do |iter|
     count, color = iter
-    game[color] = [ count.to_i, game[color] ].max
+    if count.to_i > Maximum[color]
+      return 0  # too many - this game is impossible
+    end
   end
-  { id: id.to_i, count: game }
-end
-
-def game_is_possible(game)
-  game[:count].all? do |color, count|
-    count <= Maximum[color]
-  end
+  return id.to_i
 end
 
 puts AOC.input_lines
-        .map()    { |line| parse_game(line) }
-        .reject() { |game| !game_is_possible(game) }
-        .map()    { |game| game[:id] }
+        .map()    { |line| game_id(line) }
         .sum
--- day02/part2.cr
+++ day02/part2.cr
@@ -1,28 +1,18 @@
 require "../aoc"
 
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-def parse_game(line)
-  game = {
-    "red"   => 0,
-    "green" => 0,
-    "blue"  => 0,
-  }
-
+def game_power(line)
   # Collapse all the samples together. We only care about the highest for
   # each color.
-  _, id, *samples = line.split(/[:;,]? /)
+  game = Hash(String, Int32).new(0)
+  _, _, *samples = line.split(/[:;,]? /)
   samples.each_slice(2) do |iter|
     count, color = iter
     game[color] = [ count.to_i, game[color] ].max
   end
-  { id: id.to_i, count: game }
-end
-
-def game_power(game)
-  game[:count].values.product
+  game.values.product
 end
 
 puts AOC.input_lines
-        .map() { |line| parse_game(line) }
-        .map() { |game| game_power(game) }
+        .map() { |line| game_power(line) }
         .sum
