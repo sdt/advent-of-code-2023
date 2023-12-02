b9363ab80d7f959bfb50969b9059630930c73509
Stephen Thirlwall <sdt@dr.com>
2023-12-02 17:58:44 +1100

    Day 2 refinements

--- day02/part1.cr
+++ day02/part1.cr
@@ -8,28 +8,25 @@ Maximum = {
 
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 def parse_game(line)
-  id = line.match!(/^Game (\d+):/)[1].to_i
-  subsets = [ ] of Hash(String, Int32)
-  line.split(": ")[1].split("; ").each do |round|
+  header, *rounds = line.split(/[:;] /)
+  id = header.match!(/^Game (\d+)/)[1].to_i
+  subsets = rounds.map do |round|
     subset = Hash(String, Int32).new
     round.split(", ").each do |pair|
-      words = pair.split(" ")
-      subset[words[1]] = words[0].to_i
+      count, color = pair.split(" ")
+      subset[color] = count.to_i
     end
-    subsets = subsets.push(subset)
+    subset
   end
   { id: id, rounds: subsets }
 end
 
 def game_is_possible(game)
-  game[:rounds].each do |round|
-    round.each do |color, count|
-      if count > Maximum[color]
-        return false
-      end
+  game[:rounds].all? do |round|
+    round.all? do |color, count|
+      count <= Maximum[color]
     end
   end
-  return true
 end
 
 puts AOC.input_lines
--- day02/part2.cr
+++ day02/part2.cr
@@ -2,20 +2,22 @@ require "../aoc"
 
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 def parse_game(line)
-  id = line.match!(/^Game (\d+):/)[1].to_i
-  subsets = [ ] of Hash(String, Int32)
-  line.split(": ")[1].split("; ").each do |round|
+  header, *rounds = line.split(/[:;] /)
+  id = header.match!(/^Game (\d+)/)[1].to_i
+  subsets = rounds.map do |round|
     subset = Hash(String, Int32).new
     round.split(", ").each do |pair|
-      words = pair.split(" ")
-      subset[words[1]] = words[0].to_i
+      count, color = pair.split(" ")
+      subset[color] = count.to_i
     end
-    subsets = subsets.push(subset)
+    subset
   end
   { id: id, rounds: subsets }
 end
 
 def game_power(game)
+  # "Project" through each color to get the counts for each, and get the max
+  # Then multiply the maximums together
   colors = [ "red", "green", "blue" ]
   colors.map { |color| game[:rounds].map( &.fetch(color, 0) ).max }
         .product
