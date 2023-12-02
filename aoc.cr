9bc332ce95a9c5855b79610dd88a4dd6f8acbbff
Stephen Thirlwall <sdt@dr.com>
2023-12-02 11:01:43 +1100

    Let's get started

---
+++ aoc.cr
@@ -0,0 +1,18 @@
+class AOC
+  if ARGV.size > 1
+    STDERR.puts "usage: #{PROGRAM_NAME} [input-file]"
+    exit 1
+  end
+
+  def self.input_filename : String
+    ARGV.size == 1 ? ARGV[0] : "input.txt"
+  end
+
+  def self.input_lines : Array(String)
+    File.read_lines(input_filename)
+  end
+end
+
+macro pt!(value)
+  puts "{{value}} # #{typeof({{value}})} => #{{{value}}}"
+end
