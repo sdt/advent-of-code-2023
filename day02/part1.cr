e54df8550d39fbbe29fd1f341f78473c68866edc
Stephen Thirlwall <sdt@dr.com>
2023-12-03 11:17:40 +1100

    Day 2 getting golfy now
    Eh, that's what Ruby seems to be for :)

--- day02/part1.cr
+++ day02/part1.cr
@@ -9,15 +9,12 @@ Maximum = {
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 def game_id(line)
   _, id, *samples = line.split(/[:;,]? /)
-  samples.each_slice(2) do |iter|
+  samples.each_slice(2).all? do |iter|
     count, color = iter
-    if count.to_i > Maximum[color]
-      return 0  # too many - this game is impossible
-    end
-  end
-  return id.to_i
+    count.to_i <= Maximum[color]
+  end ? id.to_i : 0
 end
 
 puts AOC.input_lines
-        .map()    { |line| game_id(line) }
+        .map() { |line| game_id(line) }
         .sum
--- day02/part2.cr
+++ day02/part2.cr
@@ -1,16 +1,15 @@
 require "../aoc"
 
+Base = { "red" => 0, "green" => 0, "blue" => 0 }
+
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 def game_power(line)
   # Collapse all the samples together. We only care about the highest for
   # each color.
-  game = Hash(String, Int32).new(0)
-  _, _, *samples = line.split(/[:;,]? /)
-  samples.each_slice(2) do |iter|
+  line.split(/[:;,]? /).skip(2).each_slice(2).reduce(Base) do |game, iter|
     count, color = iter
-    game[color] = [ count.to_i, game[color] ].max
-  end
-  game.values.product
+    game.merge({ color => [ count.to_i, game[color] ].max })
+  end.values.product
 end
 
 puts AOC.input_lines
