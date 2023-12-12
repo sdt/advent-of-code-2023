8a60718456d41e52ac53bbba982167cbdf826cda
Stephen Thirlwall <sdt@dr.com>
2023-12-13 10:40:43 +1100

    Day 12 micro-optimisations
    2.5 minutes down to 40 milliseconds

--- day12/part1.cr
+++ day12/part1.cr
@@ -1,9 +1,6 @@
 require "../aoc"
 
-def parse(line)
-  pattern, counts = line.split(/\s+/)
-  { pattern + ".", counts.split(',').map(&.to_i) }
-end
+N = 1
 
 enum Result
   Mismatch
@@ -11,54 +8,81 @@ enum Result
   Stop
 end
 
+class Solver
+  @pattern : String
+  @counts : Array(Int32)
+  @remaining_counts : Array(Int32)
+  @memo : Hash(Int32, Int64)
 
-def solve(pattern, counts, depth)
-  if counts.size == 0
-    if pattern.count('#') == 0
-      #puts "#{" "*depth}Solved at depth #{depth}"
-      return 1
-    else
-      #puts "#{" "*depth}Not solved at depth #{depth} with #{pattern} remaining"
-      return 0
+  def initialize(line : String)
+    pattern, counts = line.split(/\s+/)
+    @pattern = ([ pattern ] * N).join("?") + "."
+    @counts = counts.split(',').map(&.to_i) * N
+    @remaining_counts = @counts.reverse
+                               .accumulate { |sum, x| sum + x + 1 }
+                               .reverse
+                               .push(0)
+    @memo = Hash(Int32, Int64).new(-1)
+  end
+
+  def solve()
+    solve(0, 0)
+  end
+
+  # pi = pattern index, ci = counts index
+  def solve(pi : Int32, ci : Int32) : Int64
+    key = pi << 16 | ci
+    cached = @memo[key]
+    if cached == -1
+      cached = _solve(pi, ci)
+      @memo[key] = cached
     end
+    cached
   end
 
-  #puts "#{" "*depth}Solve #{pattern} with #{counts} at depth #{depth}"
-
-  count, *counts = counts
-  block = ("#" * count) + "."
-  inset = 0
-  total = 0
-
-  while block.size + inset <= pattern.size
-    case match(block, pattern, inset, depth)
-      when Result::Match
-        total += solve(pattern[inset + block.size..], counts, depth+1)
-      when Result::Stop
-        return total
+  def _solve(pi : Int32, ci : Int32) : Int64
+    #puts "Solve #{pi}/#{ci} - #{@pattern[pi..]} #{@counts[ci..]}"
+    if ci == @counts.size # no more counts remaining
+      return (pi ... @pattern.size).find { |i| @pattern[i] == '#' } ? 0.to_i64 : 1.to_i64
     end
-    inset += 1
-  end
-  total
-end
 
-def match(block, pattern, inset, depth)
-  if inset > 0 && pattern[inset-1] == '#'
-    #puts "#{" "*depth}Matching #{"."*inset}#{block} against #{pattern} STOP"
-    return Result::Stop
-  end
+    count = @counts[ci]
+    remainder = @remaining_counts[ci+1]
+    inset = 0
+    total : Int64 = 0.to_i64
 
-  block.chars.each_with_index do |b, i|
-    p = pattern[i + inset]
-    if (p != '?') && (p != b)
-      #puts "#{" "*depth}Matching #{"."*inset}#{block} against #{pattern} false"
-      return Result::Mismatch
+    while pi + count + inset + remainder <= @pattern.size
+      case match(pi, count, inset)
+        when Result::Match
+          total += solve(pi + inset + count + 1, ci+1)
+        when Result::Stop
+          return total
+      end
+      inset += 1
     end
+    total
   end
-  #puts "#{" "*depth}Matching #{"."*inset}#{block} against #{pattern} TRUE"
-  Result::Match
+
+  def match(pi, count, inset)
+    ##puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]}"
+    if inset > 0 && @pattern[pi+inset-1] == '#'
+      #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - STOP"
+      return Result::Stop
+    end
+
+    # check that count chars are all either '?' or '#'
+    (0 ... count).each do |i|
+      p = @pattern[pi+ i + inset]
+      #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - MISMATCH" if p == '.'
+      return Result::Mismatch if p == '.'
+    end
+
+    # The last pattern needs to be a '.' or '?'
+    #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - #{@pattern[pi + count + inset] == '#' ? "MISMATCH" : "MATCH"}"
+    @pattern[pi + count + inset] == '#' ? Result::Mismatch : Result::Match
+  end
 end
 
 puts AOC.input_lines
-     .map { |line| solve(*parse(line), 0) }
-     .sum
+        .map { |line| Solver.new(line).solve }
+        .sum
--- day12/part2.cr
+++ day12/part2.cr
@@ -2,72 +2,87 @@ require "../aoc"
 
 N = 5
 
-def parse(line)
-  pattern, counts = line.split(/\s+/)
-  pattern = ([ pattern ] * N).join("?")
-  { pattern + ".", counts.split(',').map(&.to_i) * N }
-end
-
 enum Result
   Mismatch
   Match
   Stop
 end
 
-Memo = Hash(String, Int64).new(-1)
+class Solver
+  @pattern : String
+  @counts : Array(Int32)
+  @remaining_counts : Array(Int32)
+  @memo : Hash(Int32, Int64)
 
-def solve(pattern, counts) : Int64
-  key = pattern + counts.to_s
-  cached = Memo[key]
-  if cached > 0
-    return cached
+  def initialize(line : String)
+    pattern, counts = line.split(/\s+/)
+    @pattern = ([ pattern ] * N).join("?") + "."
+    @counts = counts.split(',').map(&.to_i) * N
+    @remaining_counts = @counts.reverse
+                               .accumulate { |sum, x| sum + x + 1 }
+                               .reverse
+                               .push(0)
+    @memo = Hash(Int32, Int64).new(-1)
   end
-  cached = _solve(pattern, counts)
-  Memo[key] = cached
-  cached
-end
 
-def _solve(pattern, counts) : Int64
-  if counts.size == 0
-    if pattern.count('#') == 0
-      return 1.to_i64
-    else
-      return 0.to_i64
+  def solve()
+    solve(0, 0)
+  end
+
+  # pi = pattern index, ci = counts index
+  def solve(pi : Int32, ci : Int32) : Int64
+    key = pi << 16 | ci
+    cached = @memo[key]
+    if cached == -1
+      cached = _solve(pi, ci)
+      @memo[key] = cached
     end
+    cached
   end
 
-  count, *counts = counts
-  remainder = counts.size == 0 ? 0 : counts.sum + counts.size - 1
-  block = ("#" * count) + "."
-  inset = 0
-  total : Int64 = 0.to_i64
-
-  while block.size + inset + remainder <= pattern.size
-    case match(block, pattern, inset)
-      when Result::Match
-        total += solve(pattern[inset + block.size..], counts)
-      when Result::Stop
-        return total
+  def _solve(pi : Int32, ci : Int32) : Int64
+    #puts "Solve #{pi}/#{ci} - #{@pattern[pi..]} #{@counts[ci..]}"
+    if ci == @counts.size # no more counts remaining
+      return (pi ... @pattern.size).find { |i| @pattern[i] == '#' } ? 0.to_i64 : 1.to_i64
     end
-    inset += 1
-  end
-  total
-end
 
-def match(block, pattern, inset)
-  if inset > 0 && pattern[inset-1] == '#'
-    return Result::Stop
-  end
+    count = @counts[ci]
+    remainder = @remaining_counts[ci+1]
+    inset = 0
+    total : Int64 = 0.to_i64
 
-  block.chars.each_with_index do |b, i|
-    p = pattern[i + inset]
-    if (p != '?') && (p != b)
-      return Result::Mismatch
+    while pi + count + inset + remainder <= @pattern.size
+      case match(pi, count, inset)
+        when Result::Match
+          total += solve(pi + inset + count + 1, ci+1)
+        when Result::Stop
+          return total
+      end
+      inset += 1
     end
+    total
   end
-  Result::Match
+
+  def match(pi, count, inset)
+    ##puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]}"
+    if inset > 0 && @pattern[pi+inset-1] == '#'
+      #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - STOP"
+      return Result::Stop
+    end
+
+    # check that count chars are all either '?' or '#'
+    (0 ... count).each do |i|
+      p = @pattern[pi+ i + inset]
+      #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - MISMATCH" if p == '.'
+      return Result::Mismatch if p == '.'
+    end
+
+    # The last pattern needs to be a '.' or '?'
+    #puts "Match #{"." * inset}#{"#" * count}. against #{@pattern[pi..]} - #{@pattern[pi + count + inset] == '#' ? "MISMATCH" : "MATCH"}"
+    @pattern[pi + count + inset] == '#' ? Result::Mismatch : Result::Match
+  end
 end
 
 puts AOC.input_lines
-     .map { |line| puts(line) ; solve(*parse(line)) }
-     .sum
+        .map { |line| Solver.new(line).solve }
+        .sum
