2dc3af74613b06adc0a4c4317fce6b0cbc363316
Stephen Thirlwall <sdt@dr.com>
2023-12-13 18:56:56 +1100

    Day 13 part 2

--- day13/part2.cr
+++ day13/part2.cr
@@ -17,18 +17,27 @@ end
 
 def find_line_of_reflection(array)
   (1 ... array.size).to_a
-                    .find(0) { |i| is_line_of_reflection(array, i) }
+                    .find(0) { |i| is_line_of_reflection_with_flipped_bit(array, i) }
 end
 
-def is_line_of_reflection(array, i)
+def single_bit_set(n : Int32)
+  return n & -n == n
+end
+
+def is_line_of_reflection_with_flipped_bit(array, i)
   lhs = i-1
   rhs = i
+  bitflips = 0
   while lhs >= 0 && rhs < array.size
-    return false if array[lhs] != array[rhs]
+    xor = array[lhs] ^ array[rhs]
+    if xor != 0
+      return false if ! single_bit_set(xor)
+      bitflips += 1
+    end
     lhs -= 1
     rhs += 1
   end
-  true
+  bitflips == 1
 end
 
 puts AOC.input
