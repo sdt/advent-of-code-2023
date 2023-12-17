1af3a0311bd15a25c558945b7371e0dbde1486f4
Stephen Thirlwall <sdt@dr.com>
2023-12-17 14:35:26 +1100

    Day 16 part 1 with working
    :faceplam:

--- day16/part1.cr
+++ day16/part1.cr
@@ -6,10 +6,6 @@ class Vec2d
   def initialize(@x : Int32, @y : Int32)
   end
 
-  def == (that : self) : Bool
-    @x == that.@x && @y == that.@y
-  end
-
   def + (that : self) : self
     Vec2d.new(@x + that.@x, @y + that.@y)
   end
@@ -18,13 +14,16 @@ class Vec2d
     Vec2d.new(@x, @y)
   end
 
-  def_hash @x, @y
+  def to_s
+    "[#{@x} #{@y}]"
+  end
 end
 
 Direction = [
   Vec2d.new(1, 0), Vec2d.new(0, 1), Vec2d.new(-1, 0), Vec2d.new(0, -1)
 ]
 
+BearingChar = [ '>', 'v', '<', '^' ]
 enum Bearing
   East
   South
@@ -35,14 +34,18 @@ enum Bearing
   def to_vec2d
     Direction[self.value]
   end
+
+  def to_s
+    BearingChar[self.value]
+  end
 end
 
 class Beam
   def initialize(@pos : Vec2d, @dir : Bearing)
   end
 
-  def == (that : self) : Bool
-    @pos == that.@pos && @dir == that.@dir
+  def initialize(x : Int32, y : Int32, @dir : Bearing)
+    @pos = Vec2d.new(x, y)
   end
 
   def move()
@@ -61,53 +64,59 @@ class Beam
   end
 
   def split(m : Mirror) : Array(Beam)
-    if m == '.'
-      return [ self ]
-    end
-
+    c = self.clone
     case @dir
       when Bearing::East, Bearing::West
         case m
-          when '/'  then [ self.turn(-1) ]
-          when '\\' then [ self.turn(+1) ]
-          when '|'  then [ self.clone.turn(-1), self.turn(+1) ]
-          else           [ self ]
+          when '/'  then [ c.turn(-1) ]
+          when '\\' then [ c.turn(+1) ]
+          when '|'  then [ c.turn(-1), self.clone.turn(+1) ]
+          else           [ c ]
         end
 
       when Bearing::South, Bearing::North
         case m
-          when '/'  then [ self.turn(+1) ]
-          when '\\' then [ self.turn(-1) ]
-          when '-'  then [ self.clone.turn(-1), self.turn(+1) ]
-          else           [ self ]
+          when '/'  then [ c.turn(+1) ]
+          when '\\' then [ c.turn(-1) ]
+          when '-'  then [ c.turn(-1), self.clone.turn(+1) ]
+          else           [ c ]
         end
 
-      else [ self ]
+      else [ c ]
     end
   end
 
-  def_hash @pos, @dir
+  def to_s
+    "[#{@pos.@x} #{@pos.@y} #{@dir.to_s}]"
+  end
 end
 
+alias BeamKey = String
+alias PosKey  = String
+
 class Grid
-  @beams : Array(Beam)
-  @energised : Set(Vec2d)
-  @mirrors : Hash(Vec2d, Mirror)
-  @seen : Set(Beam)
+  @beam_queue : Deque(Beam)
+  @seen_beam : Set(BeamKey)
+  @energised : Set(PosKey)
+  @mirrors : Hash(PosKey, Mirror)
   @size : Vec2d
 
   def initialize(lines : Array(String))
-    @beams = [ Beam.new(Vec2d.new(-1, 0), Bearing::East) ]
-    @energised = Set(Vec2d).new
-    @mirrors = Hash(Vec2d, Mirror).new('.')
-    @seen = Set(Beam).new
+    @beam_queue = Deque(Beam).new
+    @seen_beam = Set(BeamKey).new
+
+    start = Beam.new(-1, 0, Bearing::East)
+    @beam_queue.push(start)
+
+    @energised = Set(PosKey).new
+    @mirrors = Hash(PosKey, Mirror).new('.')
     @size = Vec2d.new(lines[0].size, lines.size)
 
     lines.each_with_index do |line, y|
       line.chars.each_with_index do |char, x|
         if char != '.'
           pos = Vec2d.new(x, y)
-          @mirrors[pos] = char
+          @mirrors[pos.to_s] = char
         end
       end
     end
@@ -117,7 +126,7 @@ class Grid
     s = ""
     (0 ... @size.@y).each do |y|
       (0 ... @size.@x).each do |x|
-        s += @energised.includes?(Vec2d.new(x, y)) ? '#' : '.'
+        s += @energised.includes?(Vec2d.new(x, y).to_s) ? '#' : '.'
       end
       s += '\n'
     end
@@ -125,30 +134,30 @@ class Grid
   end
 
   def run() : Int32
-    while step
+    while !@beam_queue.empty?
+      beam = @beam_queue.shift
+      step(beam).each do |next_beam|
+        if @seen_beam.add?(next_beam.to_s)
+          @beam_queue.push(next_beam)
+        end
+      end
     end
-    return @energised.size
-  end
 
-  def step()
-    @beams = @beams.flat_map { |beam| step(beam) }
-
-    return false if @beams.size == 0
-
-    before = @energised.size
-    @energised |= @beams.map(&.@pos).to_set
-    after = @energised.size
-
-    after > before
+    @energised.size
   end
 
   def step(beam : Beam) : Array(Beam)
-    beam = beam.move()
-    if on_map?(beam.@pos) && @seen.add?(beam)
-      mirror = @mirrors[beam.@pos]
-      beam.split(mirror)
-    else
-      [ ] of Beam
+    loop do
+      beam.move()
+      return [ ] of Beam if ! on_map?(beam.@pos)
+
+      key = beam.@pos.to_s
+      @energised.add(key)
+
+      mirror = @mirrors[key]
+      if beam != '.'
+        return beam.split(mirror)
+      end
     end
   end
 
